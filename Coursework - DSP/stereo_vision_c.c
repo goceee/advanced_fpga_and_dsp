a918b5ab9123315819fc3974e0fee3d2e413b861
crazyHQ <nh15775@my.bristol.ac.uk>
2019-05-21 18:39:03 +0100

    Updated stereo_vision.c

--- Coursework - DSP/stereo_vision_c.c
+++ Coursework - DSP/stereo_vision_c.c
@@ -4,41 +4,42 @@
 // TODO: optimise this function for better performance
 void stereo_vision_c(unsigned char *L, unsigned char *R, unsigned char * restrict Disparity_Map, int Search_Range, int Radius)
 {
-
-    int i,j,k;
+    int i, j, k;
     int ii, jj;
     int Sum;
+    int rowOffset;
 
-    for (i=(Height-1)-Radius;i>=0+Radius;i--)
+    for (i = (Height - 1) - Radius; i >= Radius; i--)
     {
-        for (j=(Width-1)-Radius;j>=0+Radius;j--)
+        for (j = (Width - 1) - Radius; j >= Radius; j--)
         {
-            int Distance=0;
-            int Minimize=100000;
+            int Distance = 0;
+            int Minimize = 100000;
 
-            for (k=0;k<Search_Range;k++)
+            for (k = 0; k < Search_Range; k++)
             {
-                Sum=0;
-                if (j-Radius-k>=0)
+                Sum = 0;
+                if (j - Radius - k >= 0)
                 {
-
-                    for (ii=-Radius;ii<=+Radius;ii++)
+                    for (ii = -Radius; ii <= Radius; ii++)
                     {
-                        for (jj=-Radius;jj<=+Radius;jj++)
+                        rowOffset = (i + ii) * Width;
+                        for (jj = -Radius; jj <= Radius; jj++)
                         {
-                            Sum += abs(L[(i+ii)*Width+(j+jj)]-R[(i+ii)*Width+(j-k+jj)]);
-
+                            int colOffset = j + jj;
+                            int offset = rowOffset + colOffset;
+                            Sum += abs(L[offset] - R[offset - k]);
                         }
                     }
 
-                    if (Sum<Minimize)
+                    if (Sum < Minimize)
                     {
-                        Minimize=Sum;
-                        Distance=k;
+                        Minimize = Sum;
+                        Distance = k;
                     }
                 }
             }
-            Disparity_Map[i*Width+j]= Distance;
+            Disparity_Map[i * Width + j] = Distance;
         }
     }
 }
