a92b495324faeadca8b94501d09122b5761d64eb
Naimul Hoque <Naimul_Hoque@outlook.com>
2019-05-23 01:06:37 +0100

    Reformatted stereo_vision_c. Made a start on stereo_vision_sa. NOTE: stereo_vision_sa is incorrect. It's basically pseudo-assembly :P

--- Coursework - DSP/stereo_vision_c.c
+++ Coursework - DSP/stereo_vision_c.c
@@ -2,43 +2,40 @@
 #define Width 280
 
 // TODO: optimise this function for better performance
-void stereo_vision_c(unsigned char *L, unsigned char *R, unsigned char * restrict Disparity_Map, int Search_Range, int Radius)
+void stereo_vision_c(unsigned char *L, unsigned char *R, unsigned char *restrict Disparity_Map, int Search_Range, int Radius)
 {
-
-    int i,j,k;
+    int i, j, k;
     int ii, jj;
     int Sum;
 
-    for (i=(Height-1)-Radius;i>=0+Radius;i--)
+    for (i = (Height - 1) - Radius; i >= 0 + Radius; i--)
     {
-        for (j=(Width-1)-Radius;j>=0+Radius;j--)
+        for (j = (Width - 1) - Radius; j >= 0 + Radius; j--)
         {
-            int Distance=0;
-            int Minimize=100000;
+            int Distance = 0;
+            int Minimize = 100000;
 
-            for (k=0;k<Search_Range;k++)
+            for (k = 0; k < Search_Range; k++)
             {
-                Sum=0;
-                if (j-Radius-k>=0)
+                Sum = 0;
+                if (j - Radius - k >= 0)
                 {
-
-                    for (ii=-Radius;ii<=+Radius;ii++)
+                    for (ii = -Radius; ii <= +Radius; ii++)
                     {
-                        for (jj=-Radius;jj<=+Radius;jj++)
+                        for (jj = -Radius; jj <= +Radius; jj++)
                         {
-                            Sum += _subabs4(L[(i+ii)*Width+(j+jj)],R[(i+ii)*Width+(j-k+jj)]);
-
+                            Sum += _subabs4(L[(i + ii) * Width + (j + jj)], R[(i + ii) * Width + (j - k + jj)]);
                         }
                     }
 
-                    if (Sum<Minimize)
+                    if (Sum < Minimize)
                     {
-                        Minimize=Sum;
-                        Distance=k;
+                        Minimize = Sum;
+                        Distance = k;
                     }
                 }
             }
-            Disparity_Map[i*Width+j]= Distance;
+            Disparity_Map[i * Width + j] = Distance;
         }
     }
 }
