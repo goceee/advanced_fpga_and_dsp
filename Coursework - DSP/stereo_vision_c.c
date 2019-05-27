bfb034385bad7005a3dc8d3354debc780a26f39e
goceee <44786111+goceee@users.noreply.github.com>
2019-05-27 18:45:00 +0100

    C code: 0.0348, ASM code: 0.0496

--- Coursework - DSP/stereo_vision_c.c
+++ Coursework - DSP/stereo_vision_c.c
@@ -8,9 +8,10 @@ typedef const long long cll;
 void stereo_vision_c(const unsigned char *L, const unsigned char *R, unsigned char * restrict Disparity_Map, const int Search_Range, const int Radius)
 {
     int i,j,k;
-    for (i=0;i<=(Height-1)-Radius-Radius;i++)
+
+    for (i=0;i<Height-Radius-Radius;i++)
     {
-        for (j=0;j<=(Width-1)-Radius-Radius;j++)
+        for (j=0;j<Width-Radius-Radius;j++)
         {
             int Distance=0;
             int Minimize=100000;
@@ -31,12 +32,11 @@ void stereo_vision_c(const unsigned char *L, const unsigned char *R, unsigned ch
                 cll rb3 = _mem8_const(&R[(i+3)*Width+(j-k)]) & 0xFFFFFFFFFF;
                 cll rb4 = _mem8_const(&R[(i+4)*Width+(j-k)]) & 0xFFFFFFFFFF;
 
-                Sum += (_abs(_hill(lb0) - _hill(rb0)) + _dotpu4(_subabs4(_loll(lb0), _loll(rb0)), 0x01010101));
-                Sum += (_abs(_hill(lb1) - _hill(rb1)) + _dotpu4(_subabs4(_loll(lb1), _loll(rb1)), 0x01010101));
-                Sum += (_abs(_hill(lb2) - _hill(rb2)) + _dotpu4(_subabs4(_loll(lb2), _loll(rb2)), 0x01010101));
-                Sum += (_abs(_hill(lb3) - _hill(rb3)) + _dotpu4(_subabs4(_loll(lb3), _loll(rb3)), 0x01010101));
-                Sum += (_abs(_hill(lb4) - _hill(rb4)) + _dotpu4(_subabs4(_loll(lb4), _loll(rb4)), 0x01010101));
-
+                Sum += _abs(_hill(lb0) - _hill(rb0)) + _dotpu4(_subabs4(_loll(lb0), _loll(rb0)), 0x01010101);
+                Sum += _abs(_hill(lb1) - _hill(rb1)) + _dotpu4(_subabs4(_loll(lb1), _loll(rb1)), 0x01010101);
+                Sum += _abs(_hill(lb2) - _hill(rb2)) + _dotpu4(_subabs4(_loll(lb2), _loll(rb2)), 0x01010101);
+                Sum += _abs(_hill(lb3) - _hill(rb3)) + _dotpu4(_subabs4(_loll(lb3), _loll(rb3)), 0x01010101);
+                Sum += _abs(_hill(lb4) - _hill(rb4)) + _dotpu4(_subabs4(_loll(lb4), _loll(rb4)), 0x01010101);
                 Sum &= radiusCheck ? 0xFFFFFFFF : 0;
 
                 int sumCheck = radiusCheck && (Sum < Minimize);
